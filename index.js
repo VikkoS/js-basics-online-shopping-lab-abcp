4a288a5c387854ca027258ea78e4513c74d41b36
Vika Suhorukova <viktorya.sukhorukova@gmail.com>
2018-01-18 05:57:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -40,10 +40,11 @@ function viewCart() {
 }
 
 function total() {
-  let sum = 0;
-  for (let i = 0; i < cart.length) {
-    sum+=cart[i]
+  let summa = 0;
+  for (let i = 0; i < cart.length; i++) {
+    summa+=Object.values(cart[i]);
   }
+  return summa;
 }
 
 function removeFromCart(item) {
