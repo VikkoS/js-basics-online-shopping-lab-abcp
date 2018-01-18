eaf3e3900d362c5bb033062ae40424c6e348002a
Vika Suhorukova <viktorya.sukhorukova@gmail.com>
2018-01-18 04:25:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function viewCart() {
     console.log('Your shopping cart is empty.');
   } else {
     
-      message+=`${Object.keys(cart[0])} at $${Object.values(cart[0])}`;
+      message+=`${Object.getOwnPropertyNames(cart[0])} at $${Object.values(cart[0])}`;
     if (cart.length === 1) {
       message+=` and ${Object.keys(cart[1])} at $${Object.values(cart[1])}`;
     } else {
