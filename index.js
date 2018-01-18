ca6efedba0e04db94a48ac209299f9a02106d868
Vika Suhorukova <viktorya.sukhorukova@gmail.com>
2018-01-18 06:02:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,7 +48,9 @@ function total() {
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (let i = 0; i < cart.length; i++) {
+    if (hasOwnProperty(cart[i]))
+  }
 }
 
 function placeOrder(cardNumber) {
